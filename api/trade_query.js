21d520ffec1e8904700fb2ea1b6b3b479527a31d
bricebouazi <bbouazi@yahoo.fr>
2026-01-08 15:45:27 +0000

    keyupdate as varchar

--- api/trade_query.js
+++ api/trade_query.js
@@ -11,21 +11,13 @@ export default function handler(req, res) {
   }
 
   try {
-    const privateKeyPem = `-----BEGIN PRIVATE KEY-----
-MIICXAIBAAKBgQCogX+em5Awhl9zNoWw0ta6b7PFTMx73Hx+ECWBZykbmS7ycvmb
-JjCN82B8doP4ajKV9UqtE5YXugW05Eecg11DG8cQX4m5VQkmwEvxpzOsRvTcFh9h
-HV6DeTbwpTHx1I3Sjd1V2IbpRYVTItw1MPHxTwT/jerLlf4j1LlhPse4fwIDAQAB
-AoGABEvg/CNNRt92OZLPT9XgYbqNY990a0gQ6Iny2tzNgIWkW8wwrxMHM+dbs3C2
-JrRWe9pYQBd6wToeASG87bGvxAauJXbMVCGJcYWz+17lms7Sxb8JrkL0zYwUsH0t
-M8sY5dVPzVILYUZQe6qFf1RRU3RsApIfZQzeQ90VuiHEhUkCQQDUmjUPIgBSO9TD
-QVNV9ZCftz6a1YpMnr0BXmcgo9n8s4zXvQjjkAWfFEwukNiNbGyeL5iHJt3WBUvN
-BBQuLkZbAkEAyub5gNlBiMP3RUaCTM20BfvN7CH1+JJZtngWBjYBvHoSgo2W+hDi
-eKvcDp6594dkQCi+ymi/ShyUuJDOSpAXrQJAXL4seT5+32CkTsz3ep8WCOZaFBcl
-Lolsr+Urnax8kmUNAqu+7e5M1Xl4RjP/k6oBs/vVUNfem9dRmsy1tPOQ0wJBAIhk
-4f4ajXI5gYRBFKA6ezS2g0OjxKxW/RWq/eso7NvdF4pJUd9B8Gt74860JoDds6dp
-fG+mIEjak6LKPNJksHUCQGBZ9aUY8oxavOrkcxqLOAf/DI39+0I9PYQ/M1aJyOqD
-3bxQp+7UpJh9dieeag7qT4TQ93F3x4ZoGFM+s4I6LaI=
------END PRIVATE KEY-----`;  // PRIVATE_KEY
+    
+    const privateKeyPem = process.env.TESTKEY?.replace(/\\n/g, '\n');
+    //const privateKeyPem = process.env.PRIVATE_KEY?.replace(/\\n/g, '\n');
+
+if (!privateKeyPem.includes('BEGIN PRIVATE KEY')) {
+  throw new Error('Clé privée PEM invalide');
+}
 
     function encryptBizContent(bizContent, pem) {
       const buffer = Buffer.from(bizContent, 'utf8');
